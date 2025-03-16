d71cfa019ede6808e0006b63ea21a1913040808d
harsh-05 <harshverma09022001@gmail.com>
2025-03-16 18:47:43 +0530

    Added the buttons in the Hero Section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,8 +7,23 @@ export function HeroSection() {
           {" "}
           for developers
         </span>
-          </div>
-          <p className="max-w-4xl mt-10 text-center text-xs md:text-sm lg:text-base text-neutral-500">Empowe your creativity and bring your VR app ideas to life with our intuitive development tools. Get started today and turn your imagination into immersive reality.</p>
+      </div>
+      <p className="max-w-4xl mt-10 text-center text-xs md:text-sm lg:text-base text-neutral-500">
+        Empowe your creativity and bring your VR app ideas to life with our
+        intuitive development tools. Get started today and turn your imagination
+        into immersive reality.
+      </p>
+      <div className="flex justify-center my-10 gap-10">
+        <a href="#" className="border px-4 py-2 rounded">
+          Star for free
+        </a>
+        <a
+          href="#"
+          className="px-4 py-2 bg-gradient-to-r from-orange-500 to-orange-800 rounded"
+        >
+          Documentation
+        </a>
+      </div>
     </div>
   );
 }
